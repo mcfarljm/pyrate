87cf930a8eaefd136bc994250f8781f86661cd21
John McFarland <mcfarljm@gmail.com>
2019-12-14 16:06:48 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pyrate',
+      version='0.1',
+      description='Sports rating system',
+      author='John McFarland',
+      author_email='mcfarljm@gmail.com',
+      packages=find_packages())
