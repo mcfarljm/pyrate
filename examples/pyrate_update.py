40e5c4976f766c64401770db38814f689f83003e
John McFarland <mcfarljm@gmail.com>
2020-03-08 09:40:47 -0500

    Add examples/pyrate_update.py
    With example code for retrieving score data from Massey's site,
    fitting ratings, and saving the results to a database.

---
+++ examples/pyrate_update.py
@@ -0,0 +1,64 @@
+"""Example code to update ratings database using score and schedule data from www.masseyratings.com
+
+This example fits least squares ratings models to data for college and
+professional basketball and football.  The resulting ratings are
+stored to the specified SQLite database, which can be used to drive a
+website.
+
+For a deployed website, a script like this could be scheduled to run
+periodically, directing the database file to the pyrate/flaskr/static
+directory used by the flask website.
+
+Usage: pyrate_update.py [pyrate.db]
+
+Specify the name (with path) of the database for storing the results.
+
+"""
+
+import sqlalchemy
+import os
+import argparse
+
+from pyrate.rate import massey_data
+from pyrate.rate import leastsquares
+from pyrate.rate.gom import CappedPointDifference, PointDifference
+
+parser = argparse.ArgumentParser(prog='pyrate-update')
+parser.add_argument('db', nargs='?', default='pyrate.db', help='database to write to (default %(default)s)')
+args = parser.parse_args()
+
+
+db_path = os.path.abspath(args.db)
+engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path))
+
+def get_rating(name, massey_url, score_cap=None): 
+    if score_cap is not None:
+        gom = CappedPointDifference(score_cap)
+    else:
+        gom = PointDifference()
+
+    league = url.get_league_data()
+    lsq = leastsquares.LeastSquares(league, homecourt=True, game_outcome_measure=gom)
+    lsq.summarize()
+    lsq.display_ratings(5)
+
+    lsq.to_db(engine, name)
+
+
+url = massey_data.MasseyURL('nba2020')
+get_rating('NBA 2020', url)
+
+url = massey_data.MasseyURL('nfl2019')
+get_rating('NFL 2019', url)
+
+url = massey_data.MasseyURL('cb2020', ncaa_d1=True)
+get_rating('College Basketball 2020', url)
+
+url = massey_data.MasseyURL('cf2019', ncaa_d1=True)
+get_rating('College Football 2019', url)
+
+
+
+
+
+
